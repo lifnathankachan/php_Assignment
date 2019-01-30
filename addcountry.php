5c39e2757bdf5da51f05ef11cdd82301abc837f8
lifnathankachan <37173868+lifnathankachan@users.noreply.github.com>
2019-01-30 16:35:34 +0530

    Add files via upload

---
+++ addcountry.php
@@ -0,0 +1,15 @@
+<?php
+		include("dbcon.php");
+	$cname=$_POST['cname'];
+   $query="INSERT INTO `country`( `cname`) VALUES ('$cname')";
+
+
+	$query_exe=mysqli_query($con,$query);
+	if ($query_exe){
+		echo 1;
+		}
+	else {
+		echo 0;
+	}
+
+?>
